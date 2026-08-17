c592feff7bb7401b0dabb4a87a3336f93354f18c
antokhio <antokhio@gmail.com>
2026-08-17 12:48:10 +0300

    something wip

---
+++ vl-stride-csharp-shaderfx/src/MyStruct.cs
@@ -0,0 +1,14 @@
+﻿using Stride.Core.Mathematics;
+
+namespace VL.ShaderFXExt
+{
+    public struct MyStruct
+    {
+        Vector4 Color;
+
+        public MyStruct(Color color)
+        {
+            Color = color.ToVector4();
+        }
+    }
+}
